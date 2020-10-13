794b405d11f368c7b10e87369a8e09380d287802
Henrique Rocha <henriquersrocha@gmail.com>
2020-10-12 23:11:42 -0300

    Adiciona API para teste de desempenho

---
+++ src/BenchApi/Controllers/BenchController.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using BenchTestApp;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BenchApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BenchController : ControllerBase
+    {
+        private readonly Application app = new Application();
+
+        [HttpGet]
+        [Route("Get")]
+        public int Get()
+        {
+            return app.ObterId();
+        }
+
+        [HttpGet]
+        [Route("GetAsync")]
+        public Task<int> GetAsync()
+        {
+            return app.ObterAsync();
+        }
+
+        [HttpGet]
+        [Route("GetAsyncComAwait")]
+        public async Task<int> GetAsyncComAwait()
+        {
+            return await app.ObterAsyncComAwait();
+        }
+    }
+}
